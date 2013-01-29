092c62bf1c71d0936577f0f376ae81a46cfafe4e
Rohan Singh <rohan@spotify.com>
2013-01-29 12:16:25 +0100

    Serve static files out of public directory
    Instead of just serving up index.html, serve up any files in the public
    directory. This will allow us to have things like scripts and CSS.

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ var http = require('http'),
     uuid = require('node-uuid');
 
 var server = http.createServer(function (req, res) {
-  fs.readFile(__dirname + '/index.html',
-    function (err, data) {
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error');
-      }
+  var path = (req.url == '/') ? '/index.html' : req.url;
+  path = __dirname + '/public' + path;
+  console.log(path)
 
-      res.writeHead(200);
-      res.end(data);
-    });
+  fs.exists(path, function (exists) {
+    if (!exists) {
+      res.writeHead(404);
+      return res.end('File not found')
+    }
+
+    fs.readFile(path,
+      function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+      });
+  });
 });
 
 server.listen(8000);
