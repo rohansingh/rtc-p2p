f7bbae453de8fdb5a872f8219650d64b38e597d5
Rohan Singh <rohan@spotify.com>
2013-01-29 12:27:58 +0100

    Add dependencies for SHA1-hashing of files
    The basic idea is that we will have SHA1 hashes of files, and these will be
    used as identifiers to request files from peers.
    
    This commit adds the dependencies that are needed to do the hashing:
    
    * FileStreamer.js, for streaming files a few blocks at time.
    * sha1.js and sha1_stream.js, which actually implement SHA1.
    * worker.js, which does the SHA1 hashing of a file in a web worker.

---
+++ public/js/FileStreamer.js
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2012, Rohan Singh (rohan@washington.edu)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+/*
+ * See readme at: http://github.com/rohansingh/FileStreamer
+ */
+
+var FileStreamer = function (sliceSize, useFileReaderSync) {
+  this.sliceSize = sliceSize;
+  this.useFileReaderSync = useFileReaderSync;
+}
+
+FileStreamer.prototype.streamAsBinaryString = function (file, callback) {
+  return this.streamFile(file, callback);
+}
+
+FileStreamer.prototype.streamAsText = function (file, encoding, callback) {
+  return this.streamFile(file, callback, 'text', encoding);
+}
+
+FileStreamer.prototype.streamAsArrayBuffer = function (file, callback) {
+  return this.streamFile(file, callback, 'buffer');
+}
+
+FileStreamer.prototype.streamAsDataURL = function (file, callback) {
+  return this.streamFile(file, callback, 'url');
+}
+
+FileStreamer.prototype.streamFile = function (file, callback, readType, encoding) {
+  var fileReader;
+  if (this.useFileReaderSync) {
+    fileReader = new FileReaderSync();
+  }
+  else {
+    fileReader = new FileReader();
+  }
+
+  var position = 0;
+  var eof = false;
+
+  var sliceFunc = (file.webkitSlice) ? file.webkitSlice : file.mozSlice;
+  
+  var readFunc;
+  if (readType === 'text') {
+    readFunc = function (blob) {
+      return fileReader.readAsText.apply(this, [blob, encoding]);
+    }
+  }
+  else if (readType === 'buffer') {
+    readFunc = fileReader.readAsArrayBuffer;
+  }
+  else if (readType === 'url') {
+    readFunc = fileReader.readAsDataURL;
+  }
+  else {
+    readFunc = fileReader.readAsBinaryString;
+  }
+
+  var that = this;
+  var doRead = function () {
+    if (eof) {
+      // We've read the entire file.
+      return;
+    }
+
+    var end = position + that.sliceSize;
+    if (end >= file.size) {
+      end = file.size;
+      eof = true;
+    }
+
+    var blob = sliceFunc.apply(file, [position, end]);
+    position = end;
+
+    var result =  readFunc.apply(fileReader, [blob]);
+
+    // If we are using FileReaderSync, return the result of the read
+    // to the user's callback, and then start reading the next slice.
+    if (that.useFileReaderSync && callback.apply(file, [result, eof]) !== false) {
+      doRead();
+    }
+  };
+
+  if (!this.useFileReaderSync) {
+    fileReader.onloadend = function (event) {
+      if (callback.apply(file, [event, eof]) !== false) {
+        doRead();
+      }
+    };
+  }
+
+  doRead();
+}
+
---
+++ public/js/worker.js
@@ -0,0 +1,35 @@
+importScripts('FileStreamer.js', 'sha1.js', 'sha1_stream.js');
+
+worker = {
+  getSha1Hash: function (file, callback) {
+    var head = naked_sha1_head();
+
+    var fs = new FileStreamer(4 * 1024 * 1024, true);
+    fs.streamAsBinaryString(file, function (data, eof) {
+      var buffer = str2binb(data);
+
+      naked_sha1(buffer, data.length * 8, head);
+
+      if (eof) {
+        var hash = binb2hex(naked_sha1_tail(head));
+        callback(hash);
+      }
+    });
+  },
+};
+
+self.addEventListener('message', function (event) {
+  var message = event.data;
+
+  var respondFunc = function (result) {
+    postMessage({
+      action: message.action,
+      result: result
+    });
+  }
+
+  if (message.action === 'getSha1Hash') {
+    hashupWorker.getSha1Hash(message.file, respondFunc);
+  }
+}, false);
+
